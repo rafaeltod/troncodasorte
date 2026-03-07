a1a586a43beee47d6a5b90e890dae00cbabcae7b
Rafael Ricco <124226801+rafaeltod@users.noreply.github.com>
2026-03-07 20:44:38 +0000

    footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,16 +9,14 @@ import { mainConfig } from '@/lib/layout-config'
 import Image from 'next/image'
 import { Ticket, User, Trophy, TrendingUp, Menu, X, LogOut, Shield, Home, Plus, Instagram } from 'lucide-react'
 import { FaAdjust } from "react-icons/fa";
-import { BsWhatsapp } from 'react-icons/bs'
 
 export function Footer() {
 
   return (
     <footer className={`${mainConfig} flex justify-between items-center bg-branco dark:bg-cinza-escuro text-cinza text-1xl dark:text-branco lg:px-12 pt-8 pb-8 flex-wrap`}>
-        <p>Copyright © 2026, Tronco da sorte, Todos os direitos reservados.</p>
+        <p>Copyright © 2026, Tronco da Sorte, Todos os direitos reservados.</p>
         <ol className='flex gap-4'>
-            <li><Link href="/" className='flex dark:text-branco items-center text-1xl gap-1 hover:text-azul-claro'><Instagram className='w-[15px] h-[15px]'/>Instagram</Link></li>
-            <li><Link href="/" className='flex dark:text-branco items-center text-1xl gap-1 hover:text-azul-claro'><BsWhatsapp className='w-[15px] h-[15px]' />Whatsapp</Link></li>
+            <li><a href="https://www.instagram.com/troncodasorte/" target="_blank" rel="noopener noreferrer" className='flex dark:text-branco items-center text-1xl gap-1 hover:text-azul-claro'><Instagram className='w-[15px] h-[15px]'/>Instagram</a></li>
         </ol>
       
     </footer>
